3a6b4571a035893f3e7a2fce7bd77ac6786403b1
mangat-ram <se.mangat.ram@gmail.com>
2024-07-03 22:15:06 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  console.log(process.env.REACT_APP_APPWRITE_URL)
+
+  return (
+    <>  
+      <h1>Blogging App With Appwrite</h1>
+    </>
+  )
+}
+
+export default App
